3c8648b275ac3f5d6142988fedcb438e30d80de8
Teo Stocco <teo@zifeo.com>
2016-11-13 15:56:08 +0100

    initial commit

---
+++ hand-written-kohonen.py
@@ -0,0 +1,86 @@
+
+# coding: utf-8
+
+# # Kohonen maps on hand-written digits
+
+# In[3]:
+
+import numpy as np
+import matplotlib.pyplot as plt
+from helpers import *
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
